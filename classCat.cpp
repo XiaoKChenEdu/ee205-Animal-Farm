9c9d03ee5b2339e9605c1e2d313694805d1491c0
Xiaokang Chen <xiaokang.chen888@gmail.com>
2022-04-05 15:41:55 -1000

    Created classCat.cpp/.h

---
+++ classCat.cpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file classCat.cpp
+/// @version 1.0
+///
+/// @author Xiaokang Chen <xiaokang@hawaii.edu>
+/// @date   05_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include "classCat.h"
---
+++ classCat.h
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file classCat.h
+/// @version 1.0
+///
+/// @author Xiaokang Chen <xiaokang@hawaii.edu>
+/// @date   05_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+
+
+class classCat {
+
+};
