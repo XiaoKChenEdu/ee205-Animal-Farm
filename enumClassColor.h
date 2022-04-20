27185fef98a0b7f166ab90c4a6708d0c102d1264
Xiaokang Chen <xiaokang.chen888@gmail.com>
2022-04-19 17:57:25 -1000

    Finished enumClassGender.h / enumClassColor.h

---
+++ enumClassColor.h
@@ -0,0 +1,55 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-Animal-Farm - EE 205 - Spr 2022
+///
+/// @file enumClassColor.h
+/// @version 1.0
+///
+/// @author Xiaokang Chen <xiaokang@hawaii.edu>
+/// @date   19_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+
+
+#include "config.h"
+
+
+enum class Color { BLACK,
+                   WHITE,
+                   RED,
+                   BLUE,
+                   GREEN,
+                   PINK   } ;
+
+inline ostream &operator<<( ostream &lhs_stream, const Color &rhs_Color ) {
+
+    switch ( rhs_Color ) {
+
+        case Color::BLACK : lhs_stream << "Black" ;
+            break;
+
+        case Color::WHITE : lhs_stream << "White" ;
+            break;
+
+        case Color::RED   : lhs_stream << "Red"   ;
+            break;
+
+        case Color::BLUE  : lhs_stream << "Blue"  ;
+            break;
+
+        case Color::GREEN : lhs_stream << "Green" ;
+            break;
+
+        case Color::PINK  : lhs_stream << "Pink"  ;
+            break;
+
+        default:
+            throw out_of_range( "Color not mapped to a string" ) ;
+
+    }
+
+    return lhs_stream;
+
+}
