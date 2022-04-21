d8b8804689805c2fa87c247df4f691eb87b0d050
Xiaokang Chen <xiaokang.chen888@gmail.com>
2022-04-20 18:05:08 -1000

    Stubbed out classAnimal.

--- classAnimal.h
+++ classAnimal.h
@@ -36,6 +36,50 @@ class classAnimal : public classNode {
 
     public:
         /// Constructor ///
-
+        classAnimal( const classWeight::t_weight newMaxWeight,
+                     const string &NewClassification,
+                     const string &NewSpecies                  ) ;
+        classAnimal( const Gender newGender,
+                     const classWeight::t_weight newWeight,
+                     const classWeight::t_weight newMaxWeight,
+                     const string &NewClassification,
+                     const string &newSpecies                  ) ;
         /// Constructor ///
+
+    public:
+        /// Getters ///
+        string                getKingdom        () const noexcept ;
+        string                getClassification () const noexcept ;
+        string                getSpecies        () const noexcept ;
+        Gender                getGender         () const noexcept ;
+        classWeight::t_weight getWeight         () const noexcept ;
+        /// Getters ///
+
+    public:
+        /// Setters ///
+        void setWeight( const classWeight::t_weight newWeight ) ;
+        /// Setters ///
+
+    protected:
+        /// Setters ///
+        void setGender( const Gender newGender ) ;
+        /// Setters ///
+
+    public:
+        /// Static Public Member Functions ///
+        static bool validateClassification ( const string &checkClassification ) noexcept ;
+        static bool validateSpecies        ( const string &checkSpecies        ) noexcept ;
+        /// Static Public Member Functions ///
+
+    public:
+        /// Validation & Print ///
+        void print    () const noexcept override ;
+        bool validate () const noexcept override ;
+        /// Validation & Print ///
+
+    public:
+        /// Abstract Function ///
+        virtual string speak() const noexcept = 0 ;
+        /// Abstract Function ///
+
 } ;
